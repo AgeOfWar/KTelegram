359093f58e0c1fd44485695f5ecdd7a080f2a08d
Michi Palazzo <palazzomichi@gmail.com>
2021-03-06 00:46:53 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,14 @@ repositories {
 }
 
 kotlin {
-    jvm()
+    jvm() {
+        withJava()
+        val jvmJar by tasks.getting(org.gradle.jvm.tasks.Jar::class) {
+            doFirst {
+                from(configurations.getByName("runtimeClasspath").map { if (it.isDirectory) it else zipTree(it) })
+            }
+        }
+    }
     //mingwX64()
     //linuxX64()
 
