91b714f73a2b91c315c6739dd06db57edb622f46
Michi Palazzo <palazzomichi@gmail.com>
2021-05-04 23:35:06 +0200

    Add more long polling options

--- src/commonMain/kotlin/com/github/ageofwar/ktelegram/LongPollingBot.kt
+++ src/commonMain/kotlin/com/github/ageofwar/ktelegram/LongPollingBot.kt
@@ -5,24 +5,26 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ClosedSendChannelException
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.serialization.SerializationException
+import kotlin.reflect.KClass
 
 @OptIn(ExperimentalCoroutinesApi::class)
 suspend fun getPreviousUpdates(
     api: TelegramApi,
     channel: SendChannel<Update>,
+    allowedUpdates: Array<KClass<out Update>> = emptyArray(),
     exceptionHandler: suspend (Throwable) -> Unit = { it.printStackTrace() }
 ): Long? {
     var offset: Long? = null
     while (!channel.isClosedForSend) {
         try {
-            val updates = api.getUpdates(offset)
+            val updates = api.getUpdates(offset, allowedUpdates = allowedUpdates)
             if (updates.isEmpty()) break
             updates.forEach {
                 channel.send(it)
                 offset = it.id + 1
             }
         } catch (e: CancellationException) {
-            api.getUnknownUpdates(offset)
+            api.getUnknownUpdates(offset, allowedUpdates = allowedUpdates)
             throw e
         } catch (e: ClosedSendChannelException) {
             break
@@ -40,8 +42,9 @@ suspend fun getPreviousUpdates(
 suspend fun getPreviousUpdates(
     api: TelegramApi,
     channel: SendChannel<Update>,
+    allowedUpdates: Array<KClass<out Update>> = emptyArray(),
     exceptionHandlers: Array<ExceptionHandler>
-) = getPreviousUpdates(api, channel) {
+) = getPreviousUpdates(api, channel, allowedUpdates) {
     exceptionHandlers.handle(it)
 }
 
@@ -61,6 +64,7 @@ suspend fun getUpdates(
     channel: SendChannel<Update>,
     offset: Long? = null,
     backOff: (Long) -> Long = { _ -> 50L },
+    allowedUpdates: Array<KClass<out Update>> = emptyArray(),
     exceptionHandler: suspend (Throwable) -> Unit = { it.printStackTrace() }
 ) {
     @Suppress("NAME_SHADOWING")
@@ -68,7 +72,7 @@ suspend fun getUpdates(
     var attempts = 0L
     while (!channel.isClosedForSend) {
         try {
-            val updates = api.getUpdates(offset)
+            val updates = api.getUpdates(offset, allowedUpdates = allowedUpdates)
             if (updates.isNotEmpty()) {
                 updates.forEach {
                     channel.send(it)
@@ -80,7 +84,7 @@ suspend fun getUpdates(
             }
             delay(backOff(attempts))
         } catch (e: CancellationException) {
-            api.getUnknownUpdates(offset)
+            api.getUnknownUpdates(offset, allowedUpdates = allowedUpdates)
             throw e
         } catch (e: ClosedSendChannelException) {
             break
@@ -99,19 +103,21 @@ suspend fun getUpdates(
     channel: SendChannel<Update>,
     offset: Long? = null,
     backOff: (Long) -> Long = { _ -> 50L },
+    allowedUpdates: Array<KClass<out Update>> = emptyArray(),
     exceptionHandlers: Array<ExceptionHandler>
-) = getUpdates(api, channel, offset, backOff) {
+) = getUpdates(api, channel, offset, backOff, allowedUpdates) {
     exceptionHandlers.handle(it)
 }
 
 fun CoroutineScope.getPreviousUpdates(
     api: TelegramApi,
+    allowedUpdates: Array<KClass<out Update>> = emptyArray(),
     exceptionHandler: suspend (Throwable) -> Unit = { it.printStackTrace() }
 ): Channel<Update> {
     val channel = Channel<Update>()
     launch {
         try {
-            getPreviousUpdates(api, channel, exceptionHandler)
+            getPreviousUpdates(api, channel, allowedUpdates, exceptionHandler)
         } finally {
             channel.close()
         }
@@ -121,8 +127,9 @@ fun CoroutineScope.getPreviousUpdates(
 
 fun CoroutineScope.getPreviousUpdates(
     api: TelegramApi,
+    allowedUpdates: Array<KClass<out Update>> = emptyArray(),
     exceptionHandlers: Array<ExceptionHandler>
-) = getPreviousUpdates(api) {
+) = getPreviousUpdates(api, allowedUpdates) {
     exceptionHandlers.handle(it)
 }
 
@@ -130,12 +137,13 @@ fun CoroutineScope.getUpdates(
     api: TelegramApi,
     offset: Long? = null,
     backOff: (Long) -> Long = { _ -> 50L },
+    allowedUpdates: Array<KClass<out Update>> = emptyArray(),
     exceptionHandler: suspend (Throwable) -> Unit = { it.printStackTrace() }
 ): Channel<Update> {
     val channel = Channel<Update>()
     launch {
         try {
-            getUpdates(api, channel, offset, backOff, exceptionHandler)
+            getUpdates(api, channel, offset, backOff, allowedUpdates, exceptionHandler)
         } finally {
             channel.close()
         }
@@ -147,7 +155,8 @@ fun CoroutineScope.getUpdates(
     api: TelegramApi,
     offset: Long? = null,
     backOff: (Long) -> Long = { _ -> 50L },
+    allowedUpdates: Array<KClass<out Update>> = emptyArray(),
     exceptionHandlers: Array<ExceptionHandler>
-) = getUpdates(api, offset, backOff) {
+) = getUpdates(api, offset, backOff, allowedUpdates) {
     exceptionHandlers.handle(it)
 }
